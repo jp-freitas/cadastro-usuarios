fd8e4a24c665c8fc6adf79bca4b523868dc1e1c7
danipapini7 <danipapini7@gmail.com>
2026-06-11 13:06:55 +0000

    Correção de erros.

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ export const db = new Database("banco.db");
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS user (
-    id INTEGER PRIMERY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     emaill TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
